0ac174f9082aa5a08ff45a3027018f9363919aec
Ozair Ahmad <ozair.isb@gmail.com>
2025-08-08 09:10:16 +0000

    Add seach feature

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "./component/search";
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <main>
       <div className="pattern"></div>
-      <div className="wrapper"></div>
-
-      <header>
-        <h1>
-          Find <span className="text-gradient">Movies</span> You'll Enjoy
-          Without the Hassle
-        </h1>
-      </header>
-      <p>Search<p/>
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
+      </div>
     </main>
   );
 };
+
 export default App;
